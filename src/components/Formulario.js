fce9c071de1fae0ec125b3245b33bee84d3ea67b
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-19 09:55:55 +0200

    Creando el formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 const Formulario = () => {
     return ( 
-    <h1>Formulario</h1>
+   <div className="bg-info">
+       <div className="container">
+           <div className="row">
+               <form
+                className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+               >
+                   <fieldset>
+                       <legend className="text-center">Buscador Letras Canciones</legend>
+
+                       <div className="row">
+                           <div className="col-md-6">1</div>
+                           <div className="col-md-6">2</div>
+                       </div>
+                   </fieldset>
+
+               </form>
+           </div>
+       </div>
+   </div>
     
     );
 }
