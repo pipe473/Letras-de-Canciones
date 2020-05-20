a545f9a8647b4ae36d1534325ec768d99cd3e777
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-20 13:25:12 +0200

    Añadiendo el return al componente, para poder utilizar un condicional en el renderizado del componente en la busqueda

--- src/components/Cancion.js
+++ src/components/Cancion.js
@@ -1,10 +1,16 @@
 import React, { Fragment } from 'react';
 
-const Cancion = ({letra}) => (
-    <Fragment>
+const Cancion = ({letra}) =>{
+
+    if (letra.length === 0) return null;
+
+    return (
+        <Fragment>
         <h2>Letra Canción</h2>
         <p className="letra">{letra}</p>
     </Fragment>
-);
+    );
+
+}
  
 export default Cancion;
