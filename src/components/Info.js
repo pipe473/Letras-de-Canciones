c317dd50390deb752644279fc773e2ac4d7a46b1
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-20 14:00:39 +0200

    Utilizando distroctoring para añadir datos a info y poderlos mostrar

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,13 +4,18 @@ const Info = ({info}) => {
 
     if (Object.keys(info).length === 0 ) return null;
 
+    const { strArtistThumb, strGenre, strBiographyES } = info;
+
     return ( 
         <div className="card border-light">
             <div className="card-header bg-primary text-light font-weight-bold">
                 Información del Artista
             </div>
             <div className="card-body">
-                <img src={info.strArtistThumb} alt="Logo Artista" />
+                <img src={strArtistThumb} alt="Logo Artista" />
+                <p className="card-text">Género: {strGenre}</p>
+                <h2 className="card-text">Biografía:</h2>
+                <p className="card-text">{strBiographyES}</p>
             </div>
         </div>
      );
