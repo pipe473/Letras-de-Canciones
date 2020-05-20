50f05477bafe15a52936d44647f707b577e11a12
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-20 13:16:51 +0200

    Comenzando a crear componente para mostrar la letra de cancion

--- src/App.js
+++ src/App.js
@@ -29,6 +29,17 @@ useEffect(() => {
       <Formulario 
         guardarBusquedaLetra={guardarBusquedaLetra}
       />
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+
+          </div>
+          <div className="col-md-6">
+            
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 }
