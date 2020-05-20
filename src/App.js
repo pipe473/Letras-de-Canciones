85dcc66e99fb7cec34e94680ca09a88d6b4f7642
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-20 12:54:22 +0200

    Previniendo que no se ejecute nada antes de añadir algo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
 
 function App() {
 
 // Definir el state
 const [ busquedaletra, guardarBusquedaLetra ] = useState({});
+
+useEffect(() => {
+  if (Object.keys(busquedaletra).length === 0) return;
+
+  console.log('no se ejecuta');
+  
+}, [busquedaletra]);
 
   return (
     <Fragment>
