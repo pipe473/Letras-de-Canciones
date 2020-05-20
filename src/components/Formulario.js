8c55b604088bded2a57cfb461032f12ac5e84b5d
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-20 12:35:27 +0200

    Añadiendo un ternario para el error

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -37,6 +37,8 @@ const buscarInformacion = e => {
    <div className="bg-info">
        <div className="container">
            <div className="row">
+               { error ? <p className="alert alert-danger text-center p-2">Todos 
+               los campos son obligatorios</p> : null }
                <form
                 onSubmit={buscarInformacion}
                 className="col card text-white bg-transparent mb-5 pt-5 pb-2"
